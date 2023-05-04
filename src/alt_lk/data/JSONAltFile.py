0947eb1b9a1948f846d60c5cd50b1a05b09b79eb
nuuuwan <nuuuwan@gmail.com>
2023-05-04 08:55:39 +0530

    cleanedup JSONAltFile

--- src/alt_lk/data/JSONAltFile.py
+++ src/alt_lk/data/JSONAltFile.py
@@ -15,10 +15,8 @@ class JSONAltFile:
     def path(self):
         lat, lng = self.latlng
         return f'data/json/N{lat:03d}.E{lng:03d}.json'
-
-    def read(self):
-        if os.path.exists(self.path):
-            log.warning(f'{self.path} exists')
+    
+    def buildAndWrite(self):
         tif = GeoTIFFFile(self.latlng)
         data = tif.get_data()
         if data is None:
@@ -27,23 +25,12 @@ class JSONAltFile:
         JSONFile(self.path).write(data)
         dim_x, dim_y = len(data), len(data[0])
         log.info(f'Wrote {self.path} ({dim_x} x {dim_y})')
+        return data
 
-    @staticmethod
-    def combine(json_alt_file_list: list):
-        data = []
-        for json_alt_file in json_alt_file_list:
-            data_item = json_alt_file.read()
-            if data_item:
-                data.extend(data_item)
-
-        combined_path = 'data/json/all.json'
-        JSONFile(combined_path).write(data)
+    def read(self):
+        if os.path.exists(self.path):
+            return JSONFile(self.path).read()
+        data = self.buildAndWrite()
+        return data
 
 
-if __name__ == '__main__':
-    json_file_list = []
-    for lat in [5, 6, 7, 8, 9]:
-        for lng in [78, 79, 80, 81]:
-            json_file = JSONAltFile((lat, lng))
-            json_file_list.append(json_file)
-    JSONAltFile.combine(json_file_list)
